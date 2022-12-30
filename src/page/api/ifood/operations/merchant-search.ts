a1917e72fdbb9c599e745048194ac6bd2366d691
vanflux <lucasabruzzi@hotmail.com>
2022-12-30 14:39:24 -0300

    Merchant search query update

--- src/page/api/ifood/operations/merchant-search.ts
+++ src/page/api/ifood/operations/merchant-search.ts
@@ -52,10 +52,48 @@ export class IfoodMerchantSearch {
         'POST',
         `https://marketplace.ifood.com.br/v2/cardstack/search/results?alias=CMS_RESULTS_MERCHANTS&latitude=${latitude}&longitude=${longitude}&channel=IFOOD&size=${this.size}&term=${this.term}`,
         {
-          "supported-headers":["OPERATION_HEADER"],
-          "supported-cards":["MERCHANT_LIST","CATALOG_ITEM_LIST","CATALOG_ITEM_LIST_V2","CATALOG_ITEM_LIST_V3","FEATURED_MERCHANT_LIST","CATALOG_ITEM_CAROUSEL","CATALOG_ITEM_CAROUSEL_V2","CATALOG_ITEM_CAROUSEL_V3","BIG_BANNER_CAROUSEL","IMAGE_BANNER","MERCHANT_LIST_WITH_ITEMS_CAROUSEL","SMALL_BANNER_CAROUSEL","NEXT_CONTENT","MERCHANT_CAROUSEL","MERCHANT_TILE_CAROUSEL","SIMPLE_MERCHANT_CAROUSEL","INFO_CARD","MERCHANT_LIST_V2","ROUND_IMAGE_CAROUSEL","BANNER_GRID","MEDIUM_IMAGE_BANNER","MEDIUM_BANNER_CAROUSEL","RELATED_SEARCH_CAROUSEL"],
-          "supported-actions":["catalog-item","merchant","page","card-content","last-restaurants","webmiddleware","reorder","search","groceries","home-tab"],
-          "feed-feature-name":"","faster-overrides":"",
+          "supported-headers":[
+             "OPERATION_HEADER"
+          ],
+          "supported-cards":[
+             "MERCHANT_LIST",
+             "CATALOG_ITEM_LIST",
+             "CATALOG_ITEM_LIST_V2",
+             "CATALOG_ITEM_LIST_V3",
+             "FEATURED_MERCHANT_LIST",
+             "CATALOG_ITEM_CAROUSEL",
+             "CATALOG_ITEM_CAROUSEL_V2",
+             "CATALOG_ITEM_CAROUSEL_V3",
+             "BIG_BANNER_CAROUSEL",
+             "IMAGE_BANNER",
+             "MERCHANT_LIST_WITH_ITEMS_CAROUSEL",
+             "SMALL_BANNER_CAROUSEL",
+             "NEXT_CONTENT",
+             "MERCHANT_CAROUSEL",
+             "MERCHANT_TILE_CAROUSEL",
+             "SIMPLE_MERCHANT_CAROUSEL",
+             "INFO_CARD",
+             "MERCHANT_LIST_V2",
+             "ROUND_IMAGE_CAROUSEL",
+             "BANNER_GRID",
+             "MEDIUM_IMAGE_BANNER",
+             "MEDIUM_BANNER_CAROUSEL",
+             "RELATED_SEARCH_CAROUSEL"
+          ],
+          "supported-actions":[
+             "catalog-item",
+             "merchant",
+             "page",
+             "card-content",
+             "last-restaurants",
+             "webmiddleware",
+             "reorder",
+             "search",
+             "groceries",
+             "home-tab"
+          ],
+          "feed-feature-name":"",
+          "faster-overrides":""
         }
       );
 
